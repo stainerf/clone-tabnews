69cac7e1434416149b0274f326cf4b407c19a2d1
Stainer Fonseca <stainer.bfonseca@gmail.com>
2025-11-11 22:44:37 +0000

    modifica o arquivo

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
 function Home() {
     return (
         <div>
-            <h1>Oh my god, it's Dracul!</h1>
+            <h1>Olá, vem sempre por aqui, gata?</h1>
+            <img src="/images/gato.png" alt="Hagrid" height={700} />
+            <p>Me passe seu ZAP agora mesmo!</p>
+
+            <input placeholder="Insira seu ZAP aqui"
+                onKeyUp={evento => {
+                    const numero = evento.target.value
+                    if (Number(numero) && numero.length === 9) {
+                        alert("Que delícia")
+                        evento.target.value = "" 
+                    }if (!Number(numero) || numero.length > 13) {
+                        alert("Não colocou um número. Já era =(")
+                        evento.target.value = "" 
+                    }
+                }}>
+            </input>
         </div>
     )
 }
