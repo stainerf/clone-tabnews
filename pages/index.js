1326b47fa8766140c850d6e06b42c88f8f420635
Stainer Fonseca <stainer.bfonseca@gmail.com>
2026-01-04 22:46:01 +0000

    atualiza pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,4 @@ function Home() {
   return <p>Jheziree, eu te amo! Se você me ama, da uma rebolada lentinha!</p>;
 }
 
-function teste() {
-  console.log();
-}
-
-function teste2() {
-  console.log("nada");
-}
 export default Home;
